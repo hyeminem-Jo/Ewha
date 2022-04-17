d82b99d2051f66cfb2c822d19842ea61a82d8f97
hyeminem-Jo <konnimey@naver.com>
2022-04-17 23:01:12 +0900

    🐛 Fix bugs on .menu-item and set active effect on .menu-item's buttons

--- js/_sidebar-nav.js
+++ js/_sidebar-nav.js
@@ -15,23 +15,24 @@ $$sidebarNavItem.forEach((item) => {
   item.addEventListener('click', activeNavItem)
 });
 
-const $$drawerMenuItem = $sidebarNav.querySelectorAll('.menu .menu-item');
+// const $$drawerMenuItem = $sidebarNav.querySelectorAll('.menu .menu-item');
+const $$drawerMenuBtn = $sidebarNav.querySelectorAll('.menu .menu-item > a');
 let currentOpenedItem = document.querySelector('.menu .menu-item.is-open');
 
-// 한탭을 두번 누른 후 다시 다른탭을 누르면 이전 탭이 안접히는 버그
 function openDrawerMenuItem() {
-  if (currentOpenedItem !== this) {
-    this.classList.toggle('is-open')
+  const $clickedBtn = this.parentNode
+  if (currentOpenedItem !== $clickedBtn) {
+    $clickedBtn.classList.toggle('is-open')
     if (currentOpenedItem !== null) {
       currentOpenedItem.classList.remove('is-open')
     }
-    currentOpenedItem = this
+    currentOpenedItem = $clickedBtn
   } else {
-    this.classList.remove('is-open')
+    $clickedBtn.classList.remove('is-open')
     currentOpenedItem = null
   }
 }
 
-$$drawerMenuItem.forEach((item) => {
+$$drawerMenuBtn.forEach((item) => {
   item.addEventListener('click', openDrawerMenuItem)
 })
