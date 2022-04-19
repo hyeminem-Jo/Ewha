01d651fc57eb0d34ad1aae0436e0ee6a62613d84
hyeminem-Jo <konnimey@naver.com>
2022-04-19 19:32:16 +0900

    🚸 Apply overflow: hidden to .sidebar  to prevent backward scrolling

--- js/_sidebar.js
+++ js/_sidebar.js
@@ -1,13 +1,16 @@
+const $body = document.querySelector('body')
 const $sidebar = document.querySelector('.sidebar')
 const $btnClose = $sidebar.querySelector('.btn-close-37')
 const $btnMenu = document.querySelector('.middle-header-sm .btn-menu-37')
 
 function openSidebar() {
   $sidebar.classList.add('is-active')
+  $body.style.overflow = 'hidden'
 }
 
 function closeSidebar() {
   $sidebar.classList.remove('is-active')
+  $body.style.overflow = 'auto'
 }
 
 $btnMenu.addEventListener('click', openSidebar)
