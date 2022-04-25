eb398cdb0a9604a823d47d05df3e1b2265163afd
hyeminem-Jo <konnimey@naver.com>
2022-04-25 18:31:12 +0900

    💄 Implement .main-history section

--- js/carousels.js
+++ js/carousels.js
@@ -13,3 +13,15 @@ const $btnPause = document.querySelector('.main-visual .tns-outer>button')
 $btnPause.addEventListener('click', function () {
   this.classList.toggle('is-start');
 })
+
+// main-history slider 구현
+const historyCarousel = tns({
+  container: '.history-carousel-slider .slider-list',
+  mode: 'gallery',
+  navContainer: '.history-carousel-thumbnail .thumbnail-list', 
+  navAsThumbnails: true,
+  controlsContainer: '.history-carousel-slider .slider-controls',
+  arrowKeys: true,
+  mouseDrag: true,
+  preventScrollOnTouch: 'auto',
+})
